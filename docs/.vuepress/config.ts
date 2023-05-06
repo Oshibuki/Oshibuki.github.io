b3e2b3ad9f2c23750edde7c28f56af9dac92c4d4
Oshibuki <qerlkkjdfalfjelj12.adf2@gmail.com>
2023-05-06 19:56:24 +0800

    init

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,8 +2,6 @@ import { defineUserConfig } from "vuepress";
 // import { webpackBundler } from "@vuepress/bundler-webpack";
 // import { defineUserConfig } from "@vuepress/cli";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
-import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
 import { path } from "@vuepress/utils";
 import theme from "./theme";
 
@@ -11,10 +9,10 @@ export default defineUserConfig({
   // 网站语言，默认为中文
   lang: "zh-CN",
   // 网站标题
-  title: "LearnData-开源笔记",
+  title: "Oshibuki的数字城堡",
   // 网站描述
   description:
-    "开源工具、效率方法、心理学探索的自我提升笔记，记录并输出一切能让自己提升的知识。",
+    "混乱世界的一点想法",
 
   // 网站路径默认为主域名。如果网站部署在子路径下，比如 xxx.com/yyy，那么 base 应该被设置为 "/yyy/"
   base: "/",
@@ -37,20 +35,10 @@ export default defineUserConfig({
   ],
 
   plugins: [
-    // algolia 全文搜索：没设置爬虫的话，需删除 docsearchPlugin 区块以使用节点搜索
-    docsearchPlugin({
-      indexName: "newzone",
-      appId: "M4EXXEZIEG",
-      apiKey: "fd8891a9c4cc21e0ef4f11bf44f7a11e",
-    }),
     // 本地搜索，删除上方 docsearchPlugin 区块后生效
     searchProPlugin({
       // 索引全部内容
       indexContent: true,
-    }),
-    // 谷歌分析 ID
-    googleAnalyticsPlugin({
-      id: "G-RWKZTY2P9R",
     }),
   ],
 });
